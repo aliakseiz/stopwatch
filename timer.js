a64d8467dfc32c2f14eb12645a74eb3dcd921657
Aliaksei Zhuk <aliaksei.zhuk@any-ware.nl>
2024-04-05 15:13:12 +0300

    Refactored timer.js for better readability and consistency

--- timer.js
+++ timer.js
@@ -6,7 +6,7 @@ export class Timer {
     constructor() {
         this.state = TimerState.STOPPED;
         this.lastUpdate = 0;
-        this.timePassed = 0;
+        this.elapsedTime = 0;
     }
 
     isRunning() {
@@ -19,40 +19,44 @@ export class Timer {
 
     start() {
         this.state = TimerState.RUNNING;
-        this.lastUpdate = new Date().getTime();
-        this.timePassed = 0;
+        this.lastUpdate = this.getTimeNow();
+        this.elapsedTime = 0;
     }
 
     pause() {
         this.state = TimerState.PAUSED;
-        this.update();
+        this.updateElapsedTime();
     }
 
     resume() {
-        this.lastUpdate = new Date().getTime();
+        this.lastUpdate = this.getTimeNow();
         this.state = TimerState.RUNNING;
     }
 
     stop() {
         this.state = TimerState.STOPPED;
         this.lastUpdate = 0;
-        this.timePassed = 0;
+        this.elapsedTime = 0;
     }
 
-    update() {
-        if (this.state == TimerState.RUNNING) {
-            let timeElapsed = (new Date().getTime() - this.lastUpdate) / 1000.0;
-            this.timePassed = Math.max(this.timePassed + timeElapsed, 0);
-            this.lastUpdate = new Date().getTime();
-
-            if (this.timePassed <= 0) {
+    updateElapsedTime() {
+        if (this.isRunning()) {
+            const currentTime = this.getTimeNow();
+            let timeElapsed = (currentTime - this.lastUpdate) / 1000.0;
+            this.elapsedTime = Math.max(this.elapsedTime + timeElapsed, 0);
+            this.lastUpdate = currentTime;
+            if (this.elapsedTime <= 0) {
                 this.state = TimerState.STOPPED;
-                this.timePassed = 0;
+                this.elapsedTime = 0;
             }
         }
     }
 
-    setTimePassed(timePassed) {
-        this.timePassed = timePassed;
+    setElapsedTime(elapsedTime) {
+        this.elapsedTime = elapsedTime;
+    }
+
+    getTimeNow() {
+        return new Date().getTime();
     }
 }
